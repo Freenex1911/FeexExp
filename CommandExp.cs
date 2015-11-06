eca038f1842ed0d90b2b8f94f371e80d24d4b155
Freenex1911 <mustermann.max1120@gmail.com>
2015-11-06 14:27:38 +0100

    Release

---
+++ CommandExp.cs
@@ -0,0 +1,110 @@
+﻿using Rocket.API;
+using Rocket.Unturned.Chat;
+using Rocket.Unturned.Player;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Freenex.EasyExp
+{
+    public class CommandExp : IRocketCommand
+    {
+        public void Execute(IRocketPlayer caller, string[] command)
+        {
+            if (command.Length == 0 || command.Length > 2)
+            {
+                return;
+            }
+
+            if (command.Length == 1)
+            {
+                if (!(caller.HasPermission("exp"))) { return; }
+                uint commandExp;
+                bool isNumeric = uint.TryParse(command[0], out commandExp);
+                if (isNumeric)
+                {
+                    UnturnedPlayer player = (UnturnedPlayer)caller;
+                    player.Experience = player.Experience + commandExp;
+                    if (!(EasyExp.Instance.Translations.Instance.Translate("experience_self_successful") == string.Empty))
+                    {
+                        UnturnedChat.Say(caller, EasyExp.Instance.Translations.Instance.Translate("experience_self_successful", commandExp), Color.yellow);
+                    }
+                }
+            }
+
+            if (command.Length == 2)
+            {
+                if (!(caller.HasPermission("exp.other"))) { return; }
+                uint commandExp;
+                bool isNumeric = uint.TryParse(command[0], out commandExp);
+                if (isNumeric)
+                {
+                    UnturnedPlayer player = UnturnedPlayer.FromName(command[1]);
+                    if (player == null)
+                    {
+                        if (!(EasyExp.Instance.Translations.Instance.Translate("experience_other_caller_not_found") == string.Empty))
+                        {
+                            UnturnedChat.Say(caller, EasyExp.Instance.Translations.Instance.Translate("experience_other_caller_not_found"), Color.yellow);
+                        }
+                        return;
+                    }
+
+                    player.Experience = player.Experience + commandExp;
+                    if (player.Id == caller.Id)
+                    {
+                        if (!(EasyExp.Instance.Translations.Instance.Translate("experience_self_successful") == string.Empty))
+                        {
+                            UnturnedChat.Say(caller, EasyExp.Instance.Translations.Instance.Translate("experience_self_successful", commandExp), Color.yellow);
+                        }
+                    }
+                    else
+                    {
+                        if (!(EasyExp.Instance.Translations.Instance.Translate("experience_other_player_msg") == string.Empty))
+                        {
+                            UnturnedChat.Say(player, EasyExp.Instance.Translations.Instance.Translate("experience_other_player_msg", commandExp, caller.DisplayName), Color.yellow);
+                        }
+                        if (!(EasyExp.Instance.Translations.Instance.Translate("experience_other_caller_successful") == string.Empty))
+                        {
+                            UnturnedChat.Say(caller, EasyExp.Instance.Translations.Instance.Translate("experience_other_caller_successful", commandExp, player.DisplayName), Color.yellow);
+                        }
+                    }
+                }
+            }
+        }
+
+        public string Help
+        {
+            get { return "Give yourself or others Experience"; }
+        }
+
+        public string Name
+        {
+            get { return "exp"; }
+        }
+
+        public string Syntax
+        {
+            get { return "<experience> <player>"; }
+        }
+
+        public bool AllowFromConsole
+        {
+            get { return false; }
+        }
+
+        public List<string> Aliases
+        {
+            get { return new List<string>(); }
+        }
+        public List<string> Permissions
+        {
+            get
+            {
+                return new List<string>()
+                {
+                    "exp",
+                    "exp.other"
+                };
+            }
+        }
+    }
+}
---
+++ EasyExp.cs
@@ -0,0 +1,78 @@
+﻿using Rocket.API;
+using Rocket.API.Collections;
+using Rocket.Core.Logging;
+using Rocket.Core.Plugins;
+using Rocket.Unturned.Chat;
+using UnityEngine;
+
+namespace Freenex.EasyExp
+{
+    public class EasyExp : RocketPlugin
+    {
+        public static EasyExp Instance;
+
+        public override TranslationList DefaultTranslations
+        {
+            get
+            {
+                return new TranslationList() {
+                    {"experience_self_successful","You gave yourself {0} experience."},
+                    {"experience_other_caller_successful","You gave {1} {0} experience."},
+                    {"experience_other_caller_not_found","Player not found."},
+                    {"experience_other_player_msg","{1} gave you {0} experience."},
+                    {"experience_onrevive_msg","You received {0} experience."},
+                    {"experience_ondeath_msg","You lost all your experience."}
+                };
+            }
+        }
+
+        protected override void Load()
+        {
+            Instance = this;
+            Rocket.Unturned.Events.UnturnedPlayerEvents.OnPlayerDeath += UnturnedPlayerEvents_OnPlayerDeath;
+            Rocket.Unturned.Events.UnturnedPlayerEvents.OnPlayerRevive += UnturnedPlayerEvents_OnPlayerRevive;
+            Logger.Log("Freenex's EasyExp has been loaded!");
+        }
+
+        protected override void Unload()
+        {
+            Instance = this;
+            Rocket.Unturned.Events.UnturnedPlayerEvents.OnPlayerDeath -= UnturnedPlayerEvents_OnPlayerDeath;
+            Rocket.Unturned.Events.UnturnedPlayerEvents.OnPlayerRevive -= UnturnedPlayerEvents_OnPlayerRevive;
+            Logger.Log("Freenex's EasyExp has been unloaded!");
+        }
+
+        void UnturnedPlayerEvents_OnPlayerRevive(Rocket.Unturned.Player.UnturnedPlayer player, Vector3 position, byte revive)
+        {
+            foreach (string playerPermission in player.GetPermissions())
+            {
+                if (playerPermission.ToLower().Contains("exp.onrevive."))
+                {
+                    uint configExp;
+                    bool isNumeric = uint.TryParse(playerPermission.Replace("exp.onrevive.", string.Empty), out configExp);
+                    if (isNumeric)
+                    {
+                        player.Experience = player.Experience + configExp;
+                        if (!(EasyExp.Instance.Translations.Instance.Translate("experience_onrevive_msg") == string.Empty))
+                        {
+                            UnturnedChat.Say(player, EasyExp.Instance.Translations.Instance.Translate("experience_onrevive_msg", configExp), Color.yellow);
+                        }
+                    }
+                    else { Logger.LogError(playerPermission + " is not numeric."); }
+                }
+            }
+        }
+
+        void UnturnedPlayerEvents_OnPlayerDeath(Rocket.Unturned.Player.UnturnedPlayer player, SDG.Unturned.EDeathCause cause, SDG.Unturned.ELimb limb, Steamworks.CSteamID murderer)
+        {
+            if (player.HasPermission("exp.deleteondeath") && (!(player.IsAdmin)))
+            {
+                player.Experience = 0;
+                if (!(EasyExp.Instance.Translations.Instance.Translate("experience_ondeath_msg") == string.Empty))
+                {
+                    UnturnedChat.Say(player, EasyExp.Instance.Translations.Instance.Translate("experience_ondeath_msg"), Color.yellow);
+                }
+            }
+        }
+    }
+}
